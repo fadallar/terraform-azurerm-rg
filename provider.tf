e0214d502d1e1118ec16f1177d35f7d27c047ac0
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2022-12-23 11:00:44 +0100

    Fixing missing azurecaf provider

---
+++ provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    azurecaf = {
+      source = "aztfmod/azurecaf"
+      version = "2.0.0-preview3"
+    }
+  }
+}
+
+provider "azurecaf" {
+  # Configuration options
+}
