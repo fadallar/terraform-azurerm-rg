e310390a1c1436d69968309c96893de72c3a483b
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2022-12-23 10:13:11 +0100

    Fixing local tags variables

--- locals-tags.tf
+++ locals-tags.tf
@@ -1,10 +1,10 @@
 locals {
   default_tags = var.default_tags_enabled ? {
-    Environment   = var.environment
-    CostCenter = var.costcenter
-    SPOC = var.spoc
-    Owner = var.owner
-    Change = var.change
-    Stack = var.stack
+    Environment   = "${var.environment}"
+    CostCenter = "${var.costcenter}"
+    SPOC = "${var.spoc}"
+    Owner = "${var.owner}"
+    Change = "${var.change}"
+    Stack = "${var.stack}"
   } : {}
 }
