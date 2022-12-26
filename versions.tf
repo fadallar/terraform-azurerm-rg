10df7b2aafa6c674461025ca4d7b480a9ab51247
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2022-12-26 08:48:58 +0100

    Removing azurecaf module usage

--- versions.tf
+++ versions.tf
@@ -5,9 +5,5 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 1.32"
     }
-    azurecaf = {
-      source  = "aztfmod/azurecaf"
-      version = "~> 1.2, >= 1.2.22"
-    }
   }
 }
